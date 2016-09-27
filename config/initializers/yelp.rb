19f460af24ed1d4c3c7bd19fab34e0ac666e4f12
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2016-09-27 13:33:09 +0200

    add keys to yelp.rb

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,9 @@
 # inside of config/initializers/yelp.rb
 
 Yelp.client.configure do |config|
-  config.consumer_key = YOUR_CONSUMER_KEY
-  config.consumer_secret = YOUR_CONSUMER_SECRET
-  config.token = YOUR_TOKEN
-  config.token_secret = YOUR_TOKEN_SECRET
+  config.consumer_key = 5V_IK4rGj6ugzL3RK7RAMg
+  config.consumer_secret = Sx1jAOTfcE3DCm2bTTqRWz1rWBY
+  config.token = pz3_d4AJJZELUuSKXHG-ITTwlxzcBu8c
+  config.token_secret = vlohWLy9g2rv9O84SI_6VanDV50
 end
+

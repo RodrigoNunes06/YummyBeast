3e2dbd130a2119b7036e55129e5a69468a4c41e7
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2016-09-27 13:40:25 +0200

    remove keys from yelp.rb, put them in ENV

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,9 +1,9 @@
 # inside of config/initializers/yelp.rb
 
 Yelp.client.configure do |config|
-  config.consumer_key = 5V_IK4rGj6ugzL3RK7RAMg
-  config.consumer_secret = Sx1jAOTfcE3DCm2bTTqRWz1rWBY
-  config.token = pz3_d4AJJZELUuSKXHG-ITTwlxzcBu8c
-  config.token_secret = vlohWLy9g2rv9O84SI_6VanDV50
+  config.consumer_key = ENV['YELP_CONSUMER_KEY']
+  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+  config.token = ENV['YELP_TOKEN']
+  config.token_secret = ENV['YELP_TOKEN_SECRET']
 end
 
